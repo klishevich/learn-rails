18619329ef4d5485c83112e71aff7d318b474aeb
klisha <mans1@ngs.ru>
2014-03-30 00:51:49 +0400

    add basic styling

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,12 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
+    flash.now[:notice] = "Welcome!"
+    flash.now[:alert] = "Be careful!"
+    render 'visitors/new'#, :layout => 'special'
+  end
+
+  def page2
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ LearnRails::Application.routes.draw do
 
   root to: 'visitors#new'
 
+  match '/page_xxx', to: 'visitors#page2', via: 'get'
+
 end
